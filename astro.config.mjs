5b8b2ac46a4c1595060f21aea24c5a741cc5280d
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2025-12-09 23:54:14 +0900

    Fix: Update site URL to correct GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://takeuchikenta.github.io',
+  site: 'https://kenta-takeuchi.github.io',
   base: '/daily-commit',
   integrations: [tailwind()],
   markdown: {
